518ca4bdfafc4453a5913b82a38e7b26fb83df19
LucioDev7 <pianettilucio7@gmail.com>
2024-08-08 20:10:54 -0300

    array para valores ingresados

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,12 @@ import React, { useState } from "react";
 
 function App() {
 
-  const [taskName, setTakeName] = useState('My first task'); //Devuelve variable en primer posicion, funcion en el segundo.
+  const [tasks, setTasks] = useState([]); //Devuelve variable en primer posicion, funcion en el segundo.
 
   const onSubmitHandler = (event) => {
     event.preventDefault();
-    setTakeName(event.target.elements.taskName.value);
+    if (!event.target.elements.taskName.value) return;
+    setTasks( [ ...tasks, event.target.elements.taskName.value] );
     event.target.elements.taskName.value = '';
 }
 
@@ -18,7 +19,7 @@ function App() {
     <Container>
       <Titulo text="ToDo-App" />
       <Formulario onSubmitHandler={onSubmitHandler}/>
-      {taskName}
+      {JSON.stringify(tasks)}
     </Container>
     </>
   );
